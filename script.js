2192a66ea69a1bc6edbdee88a18517e958f133f7
codeFabFit <fabgar14@gmail.com>
2023-11-25 22:17:13 -0500

    questions arent popping up

---
+++ script.js
@@ -0,0 +1,95 @@
+// getting buttons to work 
+
+// start the game 
+
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+// shuffle the questions 
+
+let shuffledQuestions, currentQuestionIndex  
+
+startButton.addEventListener('click', startGame)
+
+
+function startGame () {
+    console.log("started")
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+}
+
+
+function setNextQuestions () {
+    resetState()
+    showQuesiton(shuffledQuestions[currentQuestionIndex])
+}
+
+
+
+// set the questions / show the question 
+
+
+
+function showQuesiton(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    });
+        
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+// select answer 
+
+function selectAnswer (e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+
+}
+
+const questions = [
+    {
+        question: 'what is 2 + 2?',
+        answer: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    }
+]
